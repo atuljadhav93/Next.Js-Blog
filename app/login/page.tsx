a7a31e87a0c191b78f03acaf62538cf97a7cf3cc
Atul Jadhav <prathik.paip_ext@ltts.com>
2024-06-05 16:17:27 +0530

    [2] Routing and Inner Component

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Login() {
+  return (
+    <>
+      <div>Login</div>
+      <Link href="/">Home</Link>
+    </>
+  );
+}
